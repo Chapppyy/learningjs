84b5425fff06fdfeec1f3330adb189a2249dc676
Chappyy <boisonmichael@gmail.com>
2026-03-13 18:21:59 +0000

    JS Basic Maths

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log(100 + 200 + 300 + 400 + 500 + 600);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 20;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(percentage);
