e8345db22fab7d083587d1aca4fe49d6d517f62e
Chappyy <boisonmichael@gmail.com>
2026-03-14 03:36:22 +0000

    Part 2, Strings

--- script.js
+++ script.js
@@ -16,3 +16,59 @@ const actual = max - 13;
 const percentage = actual / max;
 
 console.log(percentage);
+
+
+let myName = "paul";
+ paul = "10"
+
+console.log(myName);
+console.log(paul);
+
+const string = "The revolution will not be televised.";
+console.log(string);
+
+// const badString = string;
+// console.log(badString);
+
+// const badString = string;
+// console.log(badString);
+
+const single = 'Single quotes';
+const double = "Double quotes";
+const backtick = `Backtick`;
+
+console.log(single);
+console.log(double);
+console.log(backtick);
+
+//Using an placehilder in template literal
+const name = "Kofi";
+const age = 29;
+const mess = `My name is ${name}, and i am ${age} years old.`;
+console.log(mess);
+
+//joining two strings using ${}
+const me = "Chappy";
+const sports = " is a gamer";
+const school = " and he school\'s at Ghana Communication Technology University";
+const message = `${me}${sports},${school} `;
+
+console.log(message);
+
+//breakline using `` and \n
+//Example 1
+const me1 = "Chappy";
+const sports1 = " is a gamer";
+const school1 = " and he school\'s at Ghana Communication Technology University";
+const message1 = `${me}${sports}
+${school}.`;
+
+console.log(message1);
+
+//Example 2
+const me2 = "Chappy";
+const sports2 = " is a gamer";
+const school2 = " and he school\'s at Ghana Communication Technology University";
+console.log(`${me}${sports}${school}.`);
+
+
